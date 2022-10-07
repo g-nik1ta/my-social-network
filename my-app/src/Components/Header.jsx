224e34f8538c6dff2b988aec72fa5018ca919df2
Nikita Gonohov <gonohovn@gmail.com>
2022-10-08 01:15:47 +0300

    create header components and his styles

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            hello world!
+            <div className="logo">
+                <span>Social Network</span>
+            </div>
+            <div className="menu-user">
+                <button>register</button>
+                <button>login</button>
+            </div>
         </header>
     )
 }
