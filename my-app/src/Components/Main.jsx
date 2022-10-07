4dbfc7cd4028dc7069032f2bda705aa149c8cc52
Nikita Gonohov <gonohovn@gmail.com>
2022-10-08 01:16:54 +0300

    create others components

---
+++ my-app/src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from './Navigation';
+import Section from './Section';
+
+const Main = () => {
+    return (
+        <main>
+            <Nav/>
+            <Section/>
+        </main>
+    )
+}
+
+export default Main;
